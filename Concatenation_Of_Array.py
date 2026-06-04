1eab05b206fbd5061fed831cc70752445546068f
Harshiii3110 <harshitha311006@gmail.com>
2026-06-04 10:36:33 +0530

    Add Concatenation Of Array solution

---
+++ Concatenation_Of_Array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for num in nums:
+            ans.append(num)
+        for num in nums:
+            ans.append(num)
+        return ans        
