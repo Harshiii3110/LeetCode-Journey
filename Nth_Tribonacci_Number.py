016a6fca8286b71a69d5cd21cb3bc33a2ceed9dc
Harshiii3110 <harshitha311006@gmail.com>
2026-06-25 10:25:32 +0530

    Add N-th Tribonacci Number solution using iterative DP

---
+++ Nth_Tribonacci_Number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        a, b, c = 0, 1, 1
+        for i in range(3, n + 1):
+            curr = a + b + c
+            a = b
+            b = c
+            c = curr
+        return c        
