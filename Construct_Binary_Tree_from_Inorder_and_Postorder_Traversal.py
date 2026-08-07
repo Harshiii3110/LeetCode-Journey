0d3e40ec04bbed1a618fd87c0a9d9128d1318d58
Harshiii3110 <harshitha311006@gmail.com>
2026-08-07 12:32:43 +0530

    Add Construct Binary Tree from Inorder and Postorder Traversal solution using recursion

---
+++ Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        pos = {}
+        for i, val in enumerate(inorder):
+            pos[val] = i
+        self.postIndex = len(postorder) - 1
+        def build(left, right):
+            if left > right:
+                return None
+            rootVal = postorder[self.postIndex]
+            self.postIndex -= 1
+            root = TreeNode(rootVal)
+            mid = pos[rootVal]
+            root.right = build(mid + 1, right)
+            root.left = build(left, mid - 1)
+            return root
+        return build(0, len(inorder) - 1)       
