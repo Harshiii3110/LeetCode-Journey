f58027b4bffaa83223b30299913cfac27b6319f8
Harshiii3110 <harshitha311006@gmail.com>
2026-08-14 21:45:36 +0530

    Add Last Stone Weight solution using max heap

---
+++ Last_Stone_Weight.py
@@ -0,0 +1,15 @@
+import heapq
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        heap = [-stone for stone in stones]
+        heapq.heapify(heap)
+        while len(heap) > 1:
+            y = -heapq.heappop(heap)
+            x = -heapq.heappop(heap)
+            if y != x:
+                heapq.heappush(heap, -(y - x))
+        return -heap[0] if heap else 0       
