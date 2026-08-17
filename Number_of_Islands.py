077be2dfcaf4eb72b481527cb8be5a0ef3cf3c8d
Harshiii3110 <harshitha311006@gmail.com>
2026-08-17 09:14:07 +0530

    Add Number of Islands solution using BFS

---
+++ Number_of_Islands.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        rows = len(grid)
+        cols = len(grid[0])
+        islands = 0
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    islands += 1
+                    queue = deque([(r, c)])
+                    grid[r][c] = '0'
+                    while queue:
+                        x, y = queue.popleft()
+                        for dx, dy in directions:
+                            nx = x + dx
+                            ny = y + dy
+                            if (0 <= nx < rows and
+                                0 <= ny < cols and
+                                grid[nx][ny] == '1'):
+                                grid[nx][ny] = '0'
+                                queue.append((nx, ny))
+        return islands        
