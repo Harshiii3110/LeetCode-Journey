1749f97370c4e45cd181ccf43ae17051e44fef27
Harshiii3110 <harshitha311006@gmail.com>
2026-09-08 19:38:52 +0530

    Find intersection with duplicate frequencies

---
+++ intersection_of_two_arrays_ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        freq = {}
+        # Count elements in nums1
+        for num in nums1:
+            freq[num] = freq.get(num, 0) + 1
+        result = []
+        # Find common elements using nums2
+        for num in nums2:
+            if freq.get(num, 0) > 0:
+                result.append(num)
+                freq[num] -= 1
+        return result
