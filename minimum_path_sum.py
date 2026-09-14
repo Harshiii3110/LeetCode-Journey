84f558d1dfff5fe1380ab84d46c87911e8a839bb
Harshiii3110 <harshitha311006@gmail.com>
2026-09-14 18:25:13 +0530

    Solve minimum path sum using dynamic programming

---
+++ minimum_path_sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m = len(grid)
+        n = len(grid[0])
+        dp = [0] * n
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[j] = grid[i][j]
+                elif i == 0:
+                    dp[j] = dp[j - 1] + grid[i][j]
+                elif j == 0:
+                    dp[j] = dp[j] + grid[i][j]
+                else:
+                    dp[j] = min(dp[j], dp[j - 1]) + grid[i][j]
+        return dp[n - 1]
