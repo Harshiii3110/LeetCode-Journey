cf259481bb952ea3f9495078a1241a094e09c581
Harshiii3110 <harshitha311006@gmail.com>
2026-09-14 18:26:57 +0530

    Set matrix rows and columns to zero in-place

---
+++ set_matrix_zeroes.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        # Check if first row contains zero
+        first_row_zero = False
+        for j in range(n):
+            if matrix[0][j] == 0:
+                first_row_zero = True
+                break
+        # Check if first column contains zero
+        first_col_zero = False
+        for i in range(m):
+            if matrix[i][0] == 0:
+                first_col_zero = True
+                break
+        # Use first row and first column as markers
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        # Set marked rows to zero
+        for i in range(1, m):
+            if matrix[i][0] == 0:
+                for j in range(1, n):
+                    matrix[i][j] = 0
+        # Set marked columns to zero
+        for j in range(1, n):
+            if matrix[0][j] == 0:
+                for i in range(1, m):
+                    matrix[i][j] = 0
+        # Handle first row
+        if first_row_zero:
+            for j in range(n):
+                matrix[0][j] = 0
+        # Handle first column
+        if first_col_zero:
+            for i in range(m):
+                matrix[i][0] = 0
